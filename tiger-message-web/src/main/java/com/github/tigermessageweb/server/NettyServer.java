fd14c73526d94bf7a97450d919bbd611637787b4
jiangziwen <jiangziwen@jd.com>
2020-09-27 20:11:25 +0800

    add base dependency

---
+++ tiger-message-web/src/main/java/com/github/tigermessageweb/server/NettyServer.java
@@ -0,0 +1,12 @@
+package com.github.tigermessageweb.server;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author JZWen
+ * @date 2020/9/27
+ */
+@Component
+public class NettyServer {
+
+}
