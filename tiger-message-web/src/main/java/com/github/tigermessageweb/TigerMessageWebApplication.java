e9afdef26a97552cc76efe425da48cde8496d7db
jiangziwen <jiangziwen@jd.com>
2020-09-27 19:23:09 +0800

    init project

---
+++ tiger-message-web/src/main/java/com/github/tigermessageweb/TigerMessageWebApplication.java
@@ -0,0 +1,13 @@
+package com.github.tigermessageweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigerMessageWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TigerMessageWebApplication.class, args);
+	}
+
+}
