d10634757c5a805e279888a75b9167b801be4ceb
JZWen <1763948263@qq.com>
2020-10-12 23:57:24 +0800

    base build : web request 我总是躲着你 所以我错过了你 所以我失去了你 所以我现在非常想你啊

--- tiger-message-web-center/src/main/java/com/github/tigermessageweb/TigerMessageWebApplication.java
+++ tiger-message-web-center/src/main/java/com/github/tigermessageweb/TigerMessageWebApplication.java
@@ -1,17 +1,13 @@
 package com.github.tigermessageweb;
 
-import lombok.extern.slf4j.Slf4j;
-import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
 public class TigerMessageWebApplication {
-	private static Logger log = Logger.getLogger(TigerMessageWebApplication.class.getClass());
+
 	public static void main(String[] args) {
 		SpringApplication.run(TigerMessageWebApplication.class, args);
-		log.info("321");
 	}
 
 }
